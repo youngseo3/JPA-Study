a5194462f250b4f2792f4214e917a866ba289cef
youngseo3 <wys4475@gmail.com>
2024-10-01 22:50:50 +0900

    Feat: JPA 사용하여 간단한 CRUD

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,9 +8,38 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            // CREATE
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            // READ
+            Member findMemeber = em.find(Member.class, 2L);
+            System.out.println("findMember.id = " + findMemeber.getId());
+            System.out.println("findMember.id = " + findMemeber.getName());
+
+            // UPDATE
+            findMemeber.setName("helloJPA");
+
+            // DELETE
+            em.remove(member);
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
